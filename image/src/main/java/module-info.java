227f47f227d8c5163c3b5f048e285336a8c3d69a
David <davidrlutrick@gmail.com>
2022-07-20 21:15:17 -0700

    fixed import and added dependencies for the image module

--- image/src/main/java/module-info.java
+++ image/src/main/java/module-info.java
@@ -1,3 +1,9 @@
 module image {
+    requires org.slf4j;
+    requires java.desktop;
+    requires software.amazon.awssdk.auth;
+    requires software.amazon.awssdk.core;
+    requires software.amazon.awssdk.regions;
+    requires software.amazon.awssdk.services.rekognition;
     exports com.udacity.catpoint.image.service;
 }
